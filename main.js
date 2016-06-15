64a9448e558a0c03667cb82a355931562b24d62b
FANNAR KRISTINSSON <FANNAR KRISTINSSON>
2016-06-15 20:24:19 +0000

    Added a waveform and some cleanup

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ TODO:
     * Drag and drop music files?
     * Or maybe paste soundcloud links?
     * Or spotify?
-    * Function for drawing circles
     * Extract data in a meaningful way
     * Convey feeling of music <-- EASY
-    * Clean up this filth!
+    * Clean up this filth some more!
 
 */
 
@@ -21,6 +20,7 @@ var audioSource = function(player) {
     analyser.connect(audioCtx.destination);
     var sampleAudio = function() {
         analyser.getByteFrequencyData(self.frequencyData);
+        analyser.getByteTimeDomainData(self.timeDomainData);
         // calculate an overall volume value
         var total = 0;
         for (var i = 0; i < self.frequencyData.length; i++) {
@@ -31,6 +31,7 @@ var audioSource = function(player) {
     setInterval(sampleAudio, 20);
     // public properties and methods
     this.frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    this.timeDomainData = new Uint8Array(analyser.frequencyBinCount);
     this.volume = 0;
     this.play = function() {
         player.play();
@@ -47,7 +48,7 @@ var audioSource = function(player) {
 
 var player = document.getElementById('player');
 var songs = ['IFL', 'propaganda', 'promnight', 'tobira', 'marblesoda', 'frae', 'glosoli', 'mindspun', 'lippincott', 'endlessfantasy', 'smb', 'stupidhoe'];
-// player.setAttribute('src', 'music/propaganda.mp3');
+player.setAttribute('src', 'music/propaganda.mp3');
 // player.setAttribute('src', 'music/' + songs[7] + '.mp3');
 player.setAttribute('src', 'music/' + songs[Math.floor(Math.random() * songs.length)] + '.mp3');
 
@@ -74,9 +75,9 @@ var previousMidCircleSize = minRadius;
 var frameCounter = 0;
 
 
-// Analyses and returns averages of bass, mid, and treble for each frame. Probably useless.
-// TODO: need to find a way to mold data into usable stuff that can help with conveying the 
-//       feel of the audio better with visuals.
+/*  Analyses and returns averages of bass, mid, and treble for each frame. Probably useless.
+    TODO: need to find a way to mold data into usable stuff that can help with conveying the 
+    feel of the audio better with visuals. */
 var analyseFrequencyData = function(audioSource) {
     analysedFrequencyData = {bassAvg: 0, midAvg: 0, trebleAvg: 0, totalAvg: 0};
     len = audioSource.frequencyData.length / 2 + 3;
@@ -96,11 +97,10 @@ var analyseFrequencyData = function(audioSource) {
 }
 
 var drawVisualizer = function() {
-    frameCounter += 1;
-    // analysedData = analyseFrequencyData(aSource);
-
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, width, height);
+
+    frameCounter += 1;
 
     // Make colors b/w monochromatic when paused
     if (!isPaused) {
@@ -114,22 +114,26 @@ var drawVisualizer = function() {
     angleSize = 0;
     startAngle = 0;
     endAngle = -Math.PI / 2;
-    // hueShift: Shifts the hue value of colors, speed of shifting depends on the total volume each frame
+    // hueShift: Shifts the hue of colors, speed of shifting depends on the total volume each frame
     hueShift += aSource.volume / 10000;
 
-    // I only use half of the frequency bins, because often most of the second half is barely used
-    // and makes the visualizer look stupid. I also forego the 3 lowest frequency bins because they were
-    // fairly consistently maxed out on most songs I tested on, which made for lackluster visuals.
+    drawFrequencyBars();
+    drawWaveform(aSource.timeDomainData, foregroundGradientStops[0], aSource.volume);    
+    drawMiddleCircles();
+    requestAnimationFrame(drawVisualizer);
+};
+
+var drawFrequencyBars = function () {
+    /*  I only use half of the frequency bins, because often most of the second half is barely used
+        and makes the visualizer look stupid. I also forego the 3 lowest frequency bins because they were
+        fairly consistently maxed out on most songs I tested on, which made for lackluster visuals. */
     for (var bin = aSource.frequencyData.length / 2 + 3; bin > 3; bin--) {
         if (angleSize === 0) angleSize = 2 * Math.PI / (aSource.frequencyData.length / 2);
         startAngle = endAngle;
         endAngle += angleSize;
+        var val = aSource.frequencyData[bin];        
         
-        var val = aSource.frequencyData[bin];
-
-        // ~~ Magic number land ~~
-        
-        // background
+        /* BACKGROUND FREQUENCY BARS */
         // hue: Hue of color within a certain colorRange and with a shift applied
         var hue = (val / 256 * colorRange + hueShift) % 360;
         // ColorVal: Value of color in the range of 20-60
@@ -143,27 +147,25 @@ var drawVisualizer = function() {
             ctx.arc(width / 2, height / 2, val * (backgroundRadiusAmplifier + bin / 150), startAngle, endAngle, false);
         }
         
-        
         ctx.lineTo(width / 2, height / 2);
         backgroundColor = 'hsla(' + hue % 360 + ', ' + backgroundSaturation + '%, ' + backgroundValue + '%, 0.2)';
         ctx.fillStyle = backgroundColor;
         ctx.fill();
         ctx.closePath();
 
-        //foreground
-        // Subtraction from startAngle because of some offset I didn't understand that made gaps
-        // between bars.
+        /* FOREGROUND FREQUENCY BARS */
+        /*  Subtraction from startAngle because of some offset I didn't understand that made gaps
+            between bars. */
         startAngle -= 0.005;
         var hue = (val / 256 * colorRange + hueShift) % 360;
-        // gradientHue: Color for second color stop. It has another hue shift added to it to recieve
-        //              a complementary color a third of the way round the color wheel
+        /*  gradientHue: Color for second color stop. It has another hue shift added to it to recieve
+            a complementary color a third of the way round the color wheel */
         var gradientHue = (hue + gradientHueShift) % 360;
         ctx.beginPath();
-        // The radius has a minimum value of minRadius px, with added frequency value with amplifier
-        // and has some magic number (bin/150) added to it to balance 
-        // out the difference in frequency values to make the circle more even and prettier.
-        // NOTE: It could be evened out more but it makes the colors go out of "alignment". Stupid hack
-        // ctx.arc(width / 2, height / 2, minRadius + val * radiusAmplifier, startAngle, endAngle, false);
+        /*  The radius has a minimum value of minRadius px, with added frequency value with amplifier
+            and has some magic number (bin/150) added to it to balance 
+            out the difference in frequency values to make the circle more even and prettier.
+            NOTE: It could be evened out more but it makes the colors go out of "alignment". Stupid hack */
         ctx.arc(width / 2, height / 2, minRadius + val * (radiusAmplifier + bin / 150), startAngle, endAngle, false);
         ctx.lineTo(width / 2, height / 2);
         foregroundGradientStops[0] = 'hsl(' + gradientHue + ', ' + saturation + '%, ' + 65 + '%)';
@@ -175,76 +177,71 @@ var drawVisualizer = function() {
         ctx.fill();
         ctx.closePath();
     }
+}
 
-    // Middle circle
+var drawMiddleCircles = function () {
     var middleCircleRadius = minRadius + (aSource.volume / 110);
-    ctx.beginPath();
-    // Middle circle has a minimum radius of minRadius px so that it lines up with foreground frequency bars.
-    // It's radius is dependant on the overall volume per frame divided by a magic number that I felt
-    // made it coolest, yo.
-    ctx.arc(width / 2, height / 2, middleCircleRadius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'white';
-    ctx.fill();
-    ctx.closePath();
+    /*  Middle circle has a minimum radius of minRadius px so that it lines up with foreground frequency bars.
+        It's radius is dependant on the overall volume per frame divided by a magic number that I felt
+        made it coolest, yo. */
+    
+    drawCircle(width / 2, height / 2, middleCircleRadius, 'white');
+
     if (middleCircleRadius > 30) {
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 30, 0, 2 * Math.PI, false);
-        ctx.fillStyle = foregroundGradientStops[0];
-        ctx.fill();
-        ctx.closePath();
+        drawCircle(width / 2, height / 2, middleCircleRadius - 30, foregroundGradientStops[0]);
     }
     if (middleCircleRadius > 50) {
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 40, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
-        ctx.fill();
-        ctx.closePath();
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 50, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.closePath();
+        drawCircle(width / 2, height / 2, middleCircleRadius - 40, 'rgba(255, 255, 255, 0.3)');
+        drawCircle(width / 2, height / 2, middleCircleRadius - 50, 'white');
     }
     if (middleCircleRadius > 80) {
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 80, 0, 2 * Math.PI, false);
-        ctx.fillStyle = foregroundGradientStops[1];
-        ctx.fill();
-        ctx.closePath();
+        drawCircle(width / 2, height / 2, middleCircleRadius - 80, foregroundGradientStops[1]);
+
     }
     if (middleCircleRadius > 100) {
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 90, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
-        ctx.fill();
-        ctx.closePath();
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 100, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'white';
-        ctx.fill();
-        ctx.closePath();
+        drawCircle(width / 2, height / 2, middleCircleRadius - 90, 'rgba(255, 255, 255, 0.3)');
+        drawCircle(width / 2, height / 2, middleCircleRadius - 100, 'white');
     }
 
     if (middleCircleRadius > 130) {
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 130, 0, 2 * Math.PI, false);
-        ctx.fillStyle = foregroundGradientStops[0];
-        ctx.fill();
-        ctx.closePath();
+        drawCircle(width / 2, height / 2, middleCircleRadius - 130, foregroundGradientStops[0]);
     }
 
     if (middleCircleRadius > 150) {
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, middleCircleRadius - 150, 0, 2 * Math.PI, false);
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
-        ctx.fill();
-        ctx.closePath();
+        drawCircle(width / 2, height / 2, middleCircleRadius - 150, 'rgba(255, 255, 255, 0.3)');
     }
+}
 
-    requestAnimationFrame(drawVisualizer);
-};
+var drawWaveform = function (timeDomainData, style, volume) {
+    var startHeight = height / 2 - timeDomainData[timeDomainData.length / 2];
+    var alteredTimeDomainData = timeDomainData;
+    ctx.beginPath();
+    ctx.strokeStyle = style;
+    ctx.globalAlpha = 0.3;
+    ctx.globalCompositeOperation = "lighter";
+    var minLineWidth = 2;
+    ctx.lineWidth = minLineWidth + volume / 1200;
+    ctx.moveTo(0, startHeight * val);
+    for (var i = 0; i < timeDomainData.length; i++) {
+        var interval = width / timeDomainData.length;
+        var size = 2;
+        var val = timeDomainData[i];
+        ctx.lineTo(i * interval, startHeight + val);
+    }
+    ctx.stroke();
+    ctx.closePath();
+    ctx.globalAlpha = 1;
+    ctx.globalCompositeOperation = "source-over";
+}
 
-aSource.play();
+var drawCircle = function(x, y, radius, fillstyle) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = fillstyle;
+    ctx.fill();
+    ctx.closePath();
+}
+
 
 // Pause and unpause with spacebar
 document.addEventListener("keydown", function(event) {
@@ -257,5 +254,5 @@ document.addEventListener("keydown", function(event) {
     }
 });
 
-
+aSource.play();
 drawVisualizer();
